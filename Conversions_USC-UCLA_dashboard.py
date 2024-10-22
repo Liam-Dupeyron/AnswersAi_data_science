e3c67a21432ed5d4bf12d4a8aede9603d5ed0fdd
Liam Dupeyron <dupeyron09@berkeley.edu>
2024-10-22 10:37:56 -0700

    Tool update 2

--- Conversions_USC-UCLA_dashboard.py
+++ Conversions_USC-UCLA_dashboard.py
@@ -625,14 +625,14 @@ def cancellations_demo():
     # Define the data from the query result
     tools_data = {
         "feature_used": [
-            "Mobile OCR Mode ", 
-            "No Tool Recorded", 
-            "Snapshot", 
-            "Highlight", 
-            "Summary",
-            "Automode"
+            "No_Tool_Recorded"
+            "highlight", 
+            "snapshot", 
+            "mobile ocr mode", 
+            "auto mode", 
+            "Summary"
         ],
-        "usage_count": [198269, 7133, 5805, 3249, 1609, 808]
+        "usage_count": [16355, 10888, 2732, 1219, 411]
     }
 
     # Most Used Tools Before Cancellation
